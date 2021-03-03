1ac5d792037235959969d3fd9508983b9a753b7b
Lars Krieger <lars.krieger@gmail.com>
2021-03-03 22:00:57 +0000

    update install-ubuntu, wip script

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,2 +1,19 @@
-#!/bin/zsh
-echo "Should install the enviroment for ubuntu"
+#!/bin/bash
+# POSIX command lookup example
+CMDS="zsh node docker terraform htop git tmux"
+ 
+for i in $CMDS
+do
+  # command -v will return >0 when the $i is not found
+  if command -v $i >/dev/null
+  then
+    echo "✔ $i found, skipping"
+    continue
+  else
+    echo "✘ $i not found";
+    exit 1;
+  fi
+done
+ 
+# add rest of the script as we found all bins in $PATH
+echo "Installation process done."
